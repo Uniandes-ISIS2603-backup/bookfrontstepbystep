2fb26c4525997f72b6135433a5512754f510451b
rcasalla <rcasalla@hotmail.com>
2017-03-22 08:36:51 -0500

    init Signed-off-by:rcasalla <rcasalla@hotmail.com>

--- src/main/webapp/src/modules/books/books.mod.js
+++ src/main/webapp/src/modules/books/books.mod.js
@@ -1,18 +1,27 @@
 (function (ng) {
-    var mod = ng.module("bookModule", [ 'ui.router']);
-
+    var mod = ng.module("bookModule", ['ui.router']);
     mod.constant("booksContext", "api/books");
-
     mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
             var basePath = 'src/modules/books/';
             $urlRouterProvider.otherwise("/booksList");
-
+            self = this;
             $stateProvider.state('booksList', {
                 url: '/list',
+                resolve: {
+                    books: function ($http) {
+                        return $http.get('data/books.json').then(function (response) {
+                            self.books = response.data;
+                        });
+                    }
+                },
                 views: {
                     'mainView': {
                         templateUrl: basePath + 'books.list.html'
+
                     }
+                },
+                controller: function ($scope, books) {
+                    $scope.records = books.value;
                 }
             }).state('bookCreate', {
                 url: '/create',
